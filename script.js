1fc25bc3362d200c05055fa191f77ea0880d3afd
Gomez Santiago <santygomez355@gmail.com>
2025-10-31 18:51:34 -0300

    validacion de formulario

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+
 document.addEventListener("DOMContentLoaded", () => {
+
   // Lista de imágenes del carrusel
   const imagenes = [
     "Imagenes/Rosa.webp",
@@ -26,4 +28,44 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Cambio automático cada 4 segundos
   setInterval(() => mostrarImagen(indice + 1), 4000);
+});
+
+
+// validacion de formulario
+document.getElementById("formContacto").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const nombre = document.getElementById("nombre").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const telefono = document.getElementById("telefono").value.trim();
+  const errores = {};
+   
+  // oblicaiones de caracteres
+  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const regexTel = /^[0-9]{7,15}$/;
+
+  if (!nombre) errores.nombre = "El nombre es obligatorio.";
+  if (!regexEmail.test(email)) errores.email = "Email inválido.";
+  if (!regexTel.test(telefono)) errores.telefono = "Teléfono inválido (solo números).";
+
+  document.querySelectorAll(".error").forEach(el => el.textContent = "");
+
+  if (Object.keys(errores).length > 0) {
+    for (const campo in errores) {
+      document.getElementById(`error-${campo}`).textContent = errores[campo];
+    }
+  } else {
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+    const div = document.createElement("div");
+    div.classList.add("datos-enviados");
+    div.innerHTML = `
+      <h3>Datos Enviados:</h3>
+      <p><b>Nombre:</b> ${nombre}</p>
+      <p><b>Email:</b> ${email}</p>
+      <p><b>Teléfono:</b> ${telefono}</p>
+    `;
+    resultado.appendChild(div);
+    document.getElementById("formContacto").reset();
+  }
 });
